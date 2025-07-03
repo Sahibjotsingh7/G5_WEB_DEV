657b4f49c3d8c9450a17741f4ffbe1c339d2eb0f
SAHIBJOT <sahibjot.benipal@gmail.com>
2025-07-03 09:58:22 +0530

    heloo

--- 3_7.js
+++ 3_7.js
@@ -1,33 +1,57 @@
 // callback hell example
 // callback functions are functions that are passed as arguments to other functions and are executed after the completion of the first function
-
+// callback hell is a situation where multiple callback functions are nested inside each other, making the code difficult to read and maintain
+// Callback hell example
 function walkinres(cb) {
   console.log("walkinres called");
-  setTimeout(cb,3000);
+  setTimeout(() => {
+    console.log("walkinres completed");
+    cb();
+  }, 3000);
 }
-
 
 function checkmenu(cb) {
   console.log("checkmenu called");
-  setTimeout(cb,5000);
+  setTimeout(() => {
+    console.log("checkmenu completed");
+    cb();
+  }, 5000);
 }
 
 function orderfood(cb) {
   console.log("orderfood called");
-  setTimeout(cb,2000);
+  setTimeout(() => {
+    console.log("orderfood completed");
+    cb();
+  }, 2000);
 }
 
 function havingfood(cb) {
   console.log("havingfood called");
-  setTimeout(cb,3000);
+  setTimeout(() => {
+    console.log("havingfood completed");
+    cb();
+  }, 3000);
 }
 
-
 function paybill(cb) {
-    console.log("paybill called");
-    setTimeout(cb,1000);
-    }
+  console.log("paybill called");
+  setTimeout(() => {
+    console.log("paybill completed");
+    cb();
+  }, 1000);
+}
 
-
-
-    
+// Callback hell: Nested callbacks
+console.log("Callback hell example started");
+walkinres(() => {
+  checkmenu(() => {
+    orderfood(() => {
+      havingfood(() => {
+        paybill(() => {
+          console.log("Callback hell example completed");
+        });
+      });
+    });
+  });
+});
