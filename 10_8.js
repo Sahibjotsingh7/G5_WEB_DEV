3bb951895a9604fafeacd782dd5eda04c5a81d0b
SAHIBJOT <sahibjot.benipal@gmail.com>
2025-07-10 10:43:28 +0530

    10july

--- 10_8.js
+++ 10_8.js
@@ -1,7 +1,7 @@
 // event listener for the button
 // using single event listener for the multiple child cards on a single parent container is called event delegation
 
-const newEle = document.createElement('div');
+/*const newEle = document.createElement('div');
 newEle.classList.add('card');
 const newChild1 = document.createElement('h1');
 newChild1.innerText = "Card 6";
@@ -21,7 +21,8 @@ document.querySelector('#container').addEventListener('click', function(event) {
         event.target.classList.toggle('active');
         console.log('Card clicked:', event.target.innerText);
     }
-});
+});*/
+
 
 
 
@@ -29,3 +30,31 @@ document.querySelector('#container').addEventListener('click', function(event) {
 //  make a search bar event delegation to avoid excesive api calls using set timeout or set timeinterval;
 
 
+
+ /*const searchInput = document.querySelector('#search');
+
+ let timeoutId ;
+
+ searchInput.addEventListener('input', function(event) {
+    clearTimeout(timeoutId); // Clear the previous timeout
+    timeoutId = setTimeout(() => {
+        const searchTerm = event.target.value.toLowerCase();
+        console.log('Search Term:', searchTerm);
+        // Here you can call your search function or API with the searchTerm
+    }, 2000); // Adjust the delay as needed (300ms in this case)
+ } );*/
+
+
+ for (var index = 0; index <=5; index++) {
+      setTimeout(() => {
+        console.log(index);
+      }, 1000); // Delay each log by 1 second multiplied by the index
+    
+ }
+
+ for (let index = 0; index <=5; index++) {
+      setTimeout(() => {
+        console.log(index);
+      }, 1000); // Delay each log by 1 second multiplied by the index
+    
+ }
